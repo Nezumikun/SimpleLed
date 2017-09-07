9bd92a77168613c130496a3c3999e281855d5b1b
Nezumikun <alexey.kozhevnikov@gmail.com>
2017-09-07 11:15:16 +0300

    Version 1.0.0

---
+++ SimpleLed.cpp
@@ -0,0 +1,70 @@
+#include <Arduino.h>
+#include "SimpleLed.h"
+#include "pins_arduino.h"
+
+SimpleLed::SimpleLed(int pin) {
+  this->init(pin, 255);
+}
+
+SimpleLed::SimpleLed(int pin, int pwm_value) {
+  this->init(pin, pwm_value);
+}
+
+void SimpleLed::init(int pin, int pwm_value) {
+  this->pin_number = pin;
+  this->pwm_value = (pwm_value > 255) ? 255 : pwm_value;
+  this->pwm_available = (digitalPinToTimer(pin) != NOT_ON_TIMER);
+  pinMode(this->pin_number, OUTPUT);
+  this->off();
+}
+
+void SimpleLed::on() {
+  this->mode = SIMPLELED_ON;
+  this->state = HIGH;
+  digitalWrite(this->pin_number, HIGH);
+}
+
+void SimpleLed::off() {
+  this->mode = SIMPLELED_OFF;
+  this->state = LOW;
+  digitalWrite(this->pin_number, LOW);
+}
+
+void SimpleLed::blink(unsigned int interval) {
+  this->mode = SIMPLELED_BLINK;
+  this->interval = interval;
+  this->state = (this->state == HIGH) ? LOW : HIGH;
+  this->time = millis();
+  digitalWrite(this->pin_number, this->state);
+}
+
+void SimpleLed::flash(unsigned int interval) {
+  this->mode = SIMPLELED_FLASH;
+  this->interval = interval;
+  this->state = HIGH;
+  this->time = millis();
+  digitalWrite(this->pin_number, this->state);
+}
+
+void SimpleLed::touch() {
+  if ((this->mode != SIMPLELED_BLINK) && (this->mode != SIMPLELED_FLASH)) {
+    return;
+  }
+  unsigned long now = millis();
+  if (now - this->time >= this->interval) {
+    if (this->mode == SIMPLELED_FLASH) {
+      this->state = LOW;
+      this->mode = SIMPLELED_OFF;
+      digitalWrite(this->pin_number, LOW);
+      return;
+    }
+    this->state = (this->state == HIGH) ? LOW : HIGH;
+    this->time = now;
+    if (this->pwm_available && (this->state == HIGH)) {
+      analogWrite(this->pin_number, this->pwm_value);
+    }
+    else {
+      digitalWrite(this->pin_number, this->state);
+    }
+  }
+}
---
+++ SimpleLed.h
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+#ifndef __SIMPLELED__
+#define __SIMPLELED__
+
+enum SimpleLedMode { SIMPLELED_ON, SIMPLELED_OFF, SIMPLELED_BLINK, SIMPLELED_FLASH };
+
+class SimpleLed {
+  private:
+    int               pin_number;
+    SimpleLedMode     mode = SIMPLELED_OFF;
+    unsigned int      interval = 500;
+    unsigned long     time = 0;
+    int               state = LOW;
+    int               pwm_value = 0xFF;
+    bool              pwm_available = false;
+    void init(int pin, int pwm_value);
+  public:
+    SimpleLed (int pin);
+    SimpleLed (int pin, int pwm_value);
+    void on();
+    void off();
+    int value();
+    void value(int pwm_value);
+    void blink(unsigned int interval);
+    void flash(unsigned int interval);
+    void touch();
+}
+;
+
+#endif
